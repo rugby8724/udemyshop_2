cfe421434ad4eaf2b59658b9e2aedc3233b7b07c
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-04-09 15:20:22 -0500

    Add urls.py to cart app

--- perfectcushion/cart/urls.py
+++ perfectcushion/cart/urls.py
@@ -1,2 +1,9 @@
 from django import path
 from . import views
+
+app_name='cart'
+
+urlpatterns = [
+    path('', views.cart_detail, name='cart_detail'),
+    path('add/<int:product_id>', views.add_cart, name='add_cart'),
+]
