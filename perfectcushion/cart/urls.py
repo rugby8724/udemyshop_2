820fa8a3370bd8f5f59efbc023323c4f4ad45048
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-04-09 17:24:40 -0500

    add cart_delete to cart views.py

--- perfectcushion/cart/urls.py
+++ perfectcushion/cart/urls.py
@@ -6,4 +6,6 @@ app_name='cart'
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
     path('add/<int:product_id>', views.add_cart, name='add_cart'),
+    path('remove/<int:product_id>', views.cart_remove, name='cart_remove'),
+    path('cart_delete/<int:product_id>', views.cart_delete, name='cart_delete'),
 ]
